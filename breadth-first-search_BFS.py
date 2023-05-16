6867adf9978a09f17ba99ed6f56795abb89d4b45
Lidong90 <98975055+Lidong90@users.noreply.github.com>
2023-05-16 16:59:16 -0400

    5/16

---
+++ breadth-first-search_BFS.py
@@ -0,0 +1,28 @@
+# Given the root of a binary tree, return its maximum depth.
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        counter = 0
+        # make a queue
+        q = collections.deque()
+        q.append(root)
+        if not root:
+            return 0
+        # pop out and append node/nodes in a single round
+        while len(q):
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.node.left
+                if node.right:
+                    q.node.right
+            counter += 1
+        return counter
