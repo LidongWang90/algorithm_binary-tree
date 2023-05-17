485b756cb64aa1f8f08bd84a81130826b4ae629e
Lidong90 <98975055+Lidong90@users.noreply.github.com>
2023-05-17 11:34:32 -0400

    5/17

--- breadth-first-search_BFS.py
+++ breadth-first-search_BFS.py
@@ -26,3 +26,30 @@ class Solution:
                     q.append(node.right)
             counter += 1
         return counter
+
+
+# Given a n-ary tree, find its maximum depth.
+# The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+# Nary-Tree input serialization is represented in their level order traversal,
+# each group of children is separated by the null value (See examples).
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: "Node") -> int:
+        counter = 0
+        q = collections.deque()
+        q.append(root)
+        if not root:
+            return 0
+        while len(q) != 0:
+            for i in range(len(q)):
+                node = q.popleft()
+                for i in node.children:
+                    q.append(i)
+            counter += 1
+        return counter
